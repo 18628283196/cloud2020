4c2c4bb00235374e9e0e74f45f61bc4b3d946768
18628283196 <zl546505720@163.com>
2021-01-01 19:11:20 +0800

    update pom file

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.atguigu.springcloud.controller;
 import com.atguigu.springcloud.entities.CommResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,9 +26,22 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create", payment, CommResult.class);
     }
 
+    //getForObject:返回json数据
     @GetMapping("/consumer/payment/query/{id}")
     public CommResult<Payment> getPayment(@PathVariable("id")Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/query/"+id, CommResult.class);
+    }
+
+    //getForEntity:返回ResponseEntity对象，包含了响应中的一些信息，比如响应头、响应体、响应状态
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommResult<Payment> getForEntity(@PathVariable("id")Long id){
+        ResponseEntity<CommResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/query/" + id, CommResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommResult<>(444,"操作失败");
+        }
+
     }
 
 }
